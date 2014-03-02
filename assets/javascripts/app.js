45047e27019845534fb96e6ff91eece652966b99
Jérémy Barbet <jeremgraph@gmail.com>
2014-03-02 16:02:39 +0100

    Layout and js

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -2,36 +2,57 @@
 var gui = require('nw.gui');
 var win = gui.Window.get();
 
-var path = './';
-var fs = require('fs');
-
-fs.watch(path, [], function() {
-if (location)
-  location.reload(false);
-});
-
-
-
 // Events
 $(document).ready(function() {
-  $('.close').on('click', function() {
+  // Window events
+  $('.win-close').on('click', function() {
     win.close();
   });
 
-  $('.minimize').on('click', function() {
+  $('.win-minimize').on('click', function() {
     win.minimize();
-
   });
 
-  $('.maximize').on('click', function() {
-    win.maximize();
+  // Applications events
+  $('.add-social').on('click', function() {
+    var $this = $(this);
 
+    $this.addClass('fadeOut');
+
+    setTimeout(function() {
+      $this.css('display', 'none');
+      $('.choose-social').fadeIn('400');
+      $('.choose-social').find('li').addClass('bounceIn');
+    }, 300);
   });
 
+  $('.choose-social').on('click', 'li', function() {
+    var $this = $(this);
+    var exception = $this.data('btn');
 
+    if ( exception !== 'back' ) {
+      $this.find('span').animate({
+        marginLeft: '-240px'
+      }, 400, function() {
+        $this.find('input').show();
+        $this.find('input').focus();
+      });
+
+      $this.on('submit', function(e) {
+        var _social_site = $this.data('social');
+        var _social_value = $this.find('input').val();
+
+        console.log(_social_site, _social_value);
+
+        e.preventDefault();
+      });
+    } else {
+      $('.choose-social').addClass('fadeOut');
+
+      setTimeout(function() {
+        $('.choose-social').css('display', 'none');
+      }, 300);
+    }
+  });
 
 });
-
-
-
-
