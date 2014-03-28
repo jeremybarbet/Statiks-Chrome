6c0161294303bfc66c1a7e8918eec9a6ab3f6438
Jérémy Barbet <jeremgraph@gmail.com>
2014-03-28 23:37:04 +0100

    Update api and localstorage

--- assets/javascripts/api.js
+++ assets/javascripts/api.js
@@ -1,133 +1,116 @@
+var id = $('.list-social').find('li').length;
+var lastId;
+
+function successSocialItem($this, _social_site, username, followers) {
+  id = lastId + 1;
+  lastId++;
+
+  var itemList = '<li class="' + _social_site + '" id="' + id + '"><div class="left"><h2>' + _social_site + '</h2><p>' + username + '</p></div><div class="right"><div class="nbr">' + followers + '</div><p>followers</p></div></li>';
+  var success = $('<span class="icon-check"></span>');
+
+  $this.find('input').blur();
+  $('.icon-error').remove();
+  $this.append(success);
+
+  $('.list-social').find('ul').append(itemList);
+  saveProfile[_social_site](username, itemList);
+};
+
+function failSocialItem($this) {
+  var error = $('<span class="icon-error"></span>');
+
+  $this.find('input').val('');
+  $('.icon-check').remove();
+
+  $this.append(error);
+}
+
 var api = {
   dribbble: function($this, _social_value, _social_site) {
-    var itemList = $('<li class="test"><div class="left"><h2>social</h2><p>Username</p></div><div class="right"><div class="nbr">1422</div><p>followers</p></div></li>')
-
     $.getJSON('http://api.dribbble.com/' + _social_value, function(data) {
-      var success = $('<span class="icon-check"></span>');
+      console.log('dribbble');
 
-      console.log(data.username, data.followers_count);
+      var username = data.username;
+      var followers = data.followers_count;
 
-      $this.find('input').blur();
-      $('.icon-error').remove();
-
-      $this.append(success);
-
-
-      saveProfile['dribbble'](data);
-
-
-      // Test
-      // Add data to template
-      $('.list-social').find('ul').append(itemList);
-
-      $('.test').addClass(_social_site);
-      $('.test').find('.left h2').html(_social_site);
-      $('.test').find('.left p').html(data.username);
-      
-      $('.test').find('.right .nbr').html(data.followers_count);
+      successSocialItem($this, _social_site, username, followers);
     })
     .fail(function() {
-      var error = $('<span class="icon-error"></span>');
-
-      $this.find('input').val('');
-      $('.icon-check').remove();
-
-      $this.append(error);
+      failSocialItem($this);
     });
   },
 
+  /*
+   * There isn't any way to do it with the Twitter API.
+   * So let's crawl the page and get the right followers div
+   */
 
+  twitter: function($this, _social_value, _social_site) {
+    $.ajax({
+      url: 'https://twitter.com/' + _social_value, success: function(data) {
+        console.log(data);
 
-  twitter: function(_social_value) {
-    console.log('twitter');
+        var username = _social_value;
+        var followers = $('.follower_stats').find('.js-mini-profile-stat').html();
+
+        successSocialItem($this, _social_site, username, followers);
+      }
+    });
   },
 
+  behance: function($this, _social_value, _social_site) {
+    $.getJSON('https://www.behance.net/v2/users/' + _social_value + '?api_key=pEb2TjTxS31kT7fv2TPma6WK8WF8Mlgf', function(data) {
+      console.log('behance');
+      
+      var username = data.user.username;
+      var followers = data.user.stats.followers;
 
-
-  behance: function(_social_value) {
-    console.log('behance');
+      successSocialItem($this, _social_site, username, followers);
+    })
+    .fail(function() {
+      failSocialItem($this);
+    });
   },
-
-
-
-  google: function(_social_value) {
-    console.log('google');
-  },
-
-
 
   cinqcentpx: function($this, _social_value, _social_site) {
-    var itemList = $('<li class="test"><div class="left"><h2>social</h2><p>Username</p></div><div class="right"><div class="nbr">1422</div><p>followers</p></div></li>')
-
     $.getJSON('https://api.500px.com/v1/photos?feature=user&username=' + _social_value + '&consumer_key=GKHCkl4MdEE2rCFLVeIOWbYxhgk06s69xKnUzad3', function(data) {
-      console.log(data.photos[0].user.username);
-
-      var success = $('<span class="icon-check"></span>');
-
-      console.log(data.photos[0].user.username, data.photos[0].user.followers_count);
-
-      $this.find('input').blur();
-      $('.icon-error').remove();
-
-      $this.append(success);
-
-      saveProfile['cinqcentpx'](data);
-
-      // Test
-      // Add data to template
-      $('.list-social').find('ul').append(itemList);
-
-      $('.test').addClass(_social_site);
-      $('.test').find('.left h2').html('500px'); // To fix dynamically
-      $('.test').find('.left p').html(data.photos[0].user.username);
+      console.log('500px');
       
-      $('.test').find('.right .nbr').html(data.photos[0].user.followers_count);
+      var username = data.photos[0].user.username;
+      var followers = data.photos[0].user.followers_count;
+
+      successSocialItem($this, _social_site, username, followers);
     })
     .fail(function() {
-      var error = $('<span class="icon-error"></span>');
-
-      $this.find('input').val('');
-      $('.icon-check').remove();
-
-      $this.append(error);
+      failSocialItem($this);
     });
   },
 
-
   github: function($this, _social_value, _social_site) {
-    var itemList = $('<li class="test"><div class="left"><h2>social</h2><p>Username</p></div><div class="right"><div class="nbr">1422</div><p>followers</p></div></li>')
-
     $.getJSON('https://api.github.com/users/' + _social_value, function(data) {
-      var success = $('<span class="icon-check"></span>');
+      console.log('github');
 
-      console.log(data.login, data.followers);
+      var username = data.login;
+      var followers = data.followers;
 
-      $this.find('input').blur();
-      $('.icon-error').remove();
-
-      $this.append(success);
-
-
-      saveProfile['github'](data);
-
-
-      // Test
-      // Add data to template
-      $('.list-social').find('ul').append(itemList);
-
-      $('.test').addClass(_social_site);
-      $('.test').find('.left h2').html(_social_site);
-      $('.test').find('.left p').html(data.login);
-      
-      $('.test').find('.right .nbr').html(data.followers);
+      successSocialItem($this, _social_site, username, followers);
     })
     .fail(function() {
-      var error = $('<span class="icon-error"></span>');
+      failSocialItem($this);
+    });
+  },
 
-      $this.find('input').val('');
-      $('.icon-check').remove();
+  vimeo: function($this, _social_value, _social_site) {
+    $.getJSON('http://vimeo.com/api/v2/' + _social_value + '/info.json', function(data) {
+      console.log('vimeo');
+      
+      var username = _social_value;
+      var followers = data.total_contacts;
 
-      $this.append(error);
+      successSocialItem($this, _social_site, username, followers);
+    })
+    .fail(function() {
+      failSocialItem($this);
     });
   }
 };
--- assets/javascripts/localStorage.js
+++ assets/javascripts/localStorage.js
@@ -1,44 +1,45 @@
-/*
-Check if there is some data into localStorage
-Display the good username to the associated social account
-function retrieveProfile() {
-  if (localStorage) {
-    var test = localStorage.getItem('dribbble_username');
-
-    $('.choose-social').find('li span').animate({
-      marginLeft: '-240px'
-    }, 400, function() {
-      $('.choose-social').find('li').find('input').show();
-      $('.choose-social').find('li').find('input').val(test);
-    });
-  }
-} retriveData();
-*/
+var dataArray = [];
 
 var saveProfile = {
-  dribbble: function(data) {
+  dribbble: function(username, itemList) {
     if (localStorage) {
-      localStorage.setItem('dribbble_username', data.username);
-
-      console.log(localStorage);
+      dataArray.push(username, itemList);
+      localStorage['dribbble'] = JSON.stringify(dataArray);
     }
   },
 
-  cinqcentpx: function(data) {
+  twitter: function(username, itemList) {
     if (localStorage) {
-      localStorage.setItem('500px_username', data.photos[0].user.username);
-
-      console.log(localStorage);
+      dataArray.push(username, itemList);
+      localStorage['twitter'] = JSON.stringify(dataArray);
     }
   },
 
-  github: function(data) {
+  behance: function(username, itemList) {
     if (localStorage) {
-      localStorage.setItem('github_username', data.login);
-
-      console.log(localStorage);
+      dataArray.push(username, itemList);
+      localStorage['behance'] = JSON.stringify(dataArray);
     }
   },
 
+  cinqcentpx: function(username, itemList) {
+    if (localStorage) {
+      dataArray.push(username, itemList);
+      localStorage['500px'] = JSON.stringify(dataArray);
+    }
+  },
+  
+  github: function(username, itemList) {
+    if (localStorage) {
+      dataArray.push(username, itemList);
+      localStorage['github'] = JSON.stringify(dataArray);
+    }
+  },
 
+  vimeo: function(username, itemList) {
+    if (localStorage) {
+      dataArray.push(username, itemList);
+      localStorage['vimeo'] = JSON.stringify(dataArray);
+    }
+  }
 };
