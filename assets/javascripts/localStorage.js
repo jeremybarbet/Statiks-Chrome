06a68cf42cf8f8dc976fb24d3ed9c54e50286356
JeremDsgn <jeremgraph@gmail.com>
2014-05-10 00:29:32 +0200

    Clean up file

--- assets/javascripts/localStorage.js
+++ assets/javascripts/localStorage.js
@@ -1,7 +1,7 @@
 var dataArray = {};
 
 function saveProfile(site, username, followers) {
-  if (localStorage) {
+  if (localStorage != null) {
     dataArray[site] = {
       username: username,
       followers: followers
@@ -10,75 +10,4 @@ function saveProfile(site, username, followers) {
     localStorage.setItem('user-data', JSON.stringify(dataArray));
     console.log(dataArray);
   }
-}
-
-/*
-var saveProfile = {
-  dribbble: function(username, followers) {
-    if (localStorage) {
-      dataArray.push({
-        dribbble: {
-          username : username,
-          followers : followers
-        }
-      });
-
-      localStorage.setItem('all', JSON.stringify(dataArray));
-
-      console.log(dataArray);
-
-      // dataArray['dribbble'].push(username, followers);
-
-      // dataArray.push(username, followers);
-      // localStorage['dribbble'] = JSON.stringify(dataArray);
-    }
-  },
-
-  twitter: function(username, followers) {
-    if (localStorage) {
-      dataArray.push(username, followers);
-      localStorage['twitter'] = JSON.stringify(dataArray);
-    }
-  },
-
-  behance: function(username, followers) {
-    if (localStorage) {
-
-      dataArray.push({
-        behance: {
-          username : username,
-          followers : followers
-        }
-      });
-
-      localStorage.setItem('all', JSON.stringify(dataArray));
-
-      console.log(dataArray);
-
-      // dataArray.push(username, followers);
-      // localStorage['behance'] = JSON.stringify(dataArray);
-    }
-  },
-
-  cinqcentpx: function(username, followers) {
-    if (localStorage) {
-      dataArray.push(username, followers);
-      localStorage['500px'] = JSON.stringify(dataArray);
-    }
-  },
-  
-  github: function(username, followers) {
-    if (localStorage) {
-      dataArray.push(username, followers);
-      localStorage['github'] = JSON.stringify(dataArray);
-    }
-  },
-
-  vimeo: function(username, followers) {
-    if (localStorage) {
-      dataArray.push(username, followers);
-      localStorage['vimeo'] = JSON.stringify(dataArray);
-    }
-  }
 };
-*/
