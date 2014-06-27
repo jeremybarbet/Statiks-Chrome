8a29d9f797ecbe26cb886944f8fd13bb1f0f1bd7
Jérémy Barbet <jeremgraph@gmail.com>
2014-06-27 15:03:22 +0200

    Big improving and refactoring method related to sum up followers/following data for each networks, and total.

--- assets/javascripts/appApi.js
+++ assets/javascripts/appApi.js
@@ -1,9 +1,18 @@
 /**
  * API namespace with related methods
- * Success, fail, reload and API connects
+ * Variables, success, fail, alert, store, graph, reload, reloadCount, check, upgrade and API connects
  * @global
  */
 var api = {
+  /*
+   * Global variables
+   */
+  reloadNbr: 0,
+  reloadAllow: false,
+  buildGraph: false,
+  curIndex: 0,
+  curSite: [],
+
   /**
    * Alert messages
    */
@@ -34,22 +43,9 @@ var api = {
 
     /*
     * Check if localstorage exist
-    * Push data to object and store it
+    * Call function to store data
     */
-    if ( localStorage !== null ) {
-      dataObj.sites[site] = {
-        username: username,
-        followers: followers,
-        details: details,
-        diff: {
-          value: 0,
-          followers: [0, 0, 0, 0, 0, 0, 0],
-          following: [0, 0, 0, 0, 0, 0, 0]
-        }
-      };
-
-      storage.set('user-data', dataObj);
-    }
+    if ( localStorage !== null ) api.store(dataObj, site, username, followers, details);
   },
 
   /**
@@ -77,7 +73,7 @@ var api = {
   /**
    * Alert messages
    */
-   alert: function(message) {
+  alert: function(message) {
     // Not add it on the DOM again, if is already append
     if ( !$('.alert').length ) {
       var alertWrapper = '<div class="alert"><p></p></div>';
@@ -95,14 +91,176 @@ var api = {
         opacity: '0'
       }, timingEffect);
     }, 4000);
-   },
+  },
+
+  /**
+   * Function to store data to localstorage if all is good
+   */
+  store: function(dataObj, site, username, followers, details) {
+    dataObj.sites[site] = {
+      username: username,
+      followers: followers,
+      details: details,
+      diff: {
+        value: 0,
+        followers: [0, 0, 0, 0, 0, 0, 0],
+        following: [0, 0, 0, 0, 0, 0, 0]
+      }
+    };
+
+    api.graph.sum.networks(site, followers, details);
+
+    storage.set('user-data', dataObj);
+  },
+
+  /**
+   * Clean classes to remove followers/following data on full arrays
+   */
+  graph: {
+    // Detect when arrays are full and called the related function to clean them
+    isFull: function() {
+      for (var i = 0; i < dataObj.graph.followers.length; i++) {
+        if ( isInArray(0, dataObj.graph.followers) === false ) {
+          var totalFollowers = dataObj.graph.followers[i];
+          var totalFollowing = dataObj.graph.following[i];
+
+          for (var site in dataObj.sites) {
+            var networksFollowers = dataObj.sites[site].diff.followers[i];
+            var networksFollowing = dataObj.sites[site].diff.following[i];
+
+            api.graph.clean.networks(site, networksFollowers, networksFollowing);
+          }
+
+          api.graph.clean.total(totalFollowers, totalFollowing);
+
+          break;
+        }
+      }
+    },
+
+    sum: {
+      // Add newtworks total
+      networks: function(site, followers, details) {
+        var tmp = storage.get('user-data');
+
+        // Add followers/following numbers to related's site arrays
+        for (i = 0; i < dataObj.graph.followers.length; i++) {
+          if ( dataObj.graph.followers[i] === 0 ) {
+            var curIndex = i;
+            var curSite = site;
+
+            dataObj.sites[site].diff.followers[i] = followers;
+            if ( dataObj.sites[site].details.hasOwnProperty('following') ) dataObj.sites[site].diff.following[i] = details.following;
+
+            break;
+          }
+        }
+
+        if ( tmp !== null ) {
+          // Enable the call to the function to sum up total followers/following
+          for (var key in tmp.sites) {
+            if ( !tmp.sites.hasOwnProperty(site) ) {
+              // Copy last followers/following number to related index
+              api.curIndex = curIndex;
+              api.curSite.push(site);
+
+              // Allow the build of graph data
+              api.buildGraph = true;
+
+              break;
+            }
+          }
+        } else {
+          api.buildGraph = true;
+        }
+      },
+
+      // Method to sum up total of followers and following for all networks
+      total: function() {
+        for (var i = 0; i < dataObj.graph.followers.length; i++ ) {
+          if ( dataObj.graph.followers[i] === 0 ) {
+            for (site in dataObj.sites) {
+              dataObj.graph.followers[i] += parseInt(dataObj.sites[site].diff.followers[i]);
+              dataObj.graph.following[i] += parseInt(dataObj.sites[site].diff.following[i]);
+            }
+          }
+        }
+
+        storage.set('user-data', dataObj);
+      },
+
+      // Method to sum up missing followers/following values of previous networks
+      missing: function(index, site) {
+        for (var key in dataObj.sites) {
+          for (var i = 0; i < site.length; i++) {
+            if ( key !== site[i] ) {
+              // First loop to store the value with the highest index
+              for (var i = index; i >= 0; i--) {
+                if ( isSame(dataObj.sites[key].diff.followers) === true ) var followersValue = followingValue = 0;
+
+                if ( dataObj.sites[key].diff.followers[i] !== 0 ) {
+                  var followersValue = dataObj.sites[key].diff.followers[i];
+                  var followingValue = dataObj.sites[key].diff.following[i];
+                }
+              }
+
+              // Second loop to complete and according to current index
+              for (var i = index; i >= 0; i--) {
+                if ( dataObj.sites[key].diff.followers[i] === 0 ) {
+                  dataObj.sites[key].diff.followers[i] = followersValue;
+                  if ( dataObj.sites[key].details.hasOwnProperty('following') ) dataObj.sites[key].diff.following[i] = followingValue;
+                }
+              }
+
+              storage.set('user-data', dataObj);
+
+              break;
+            }
+          }
+        }
+      },
+    },
+
+    // Clean classes to remove followers/following data on full arrays
+    clean: {
+      // Clean data for each networks
+      networks: function(site, followers, following) {
+        for (var i = 0; i < dataObj.sites[site].diff.followers.length; i++) {
+          dataObj.sites[site].diff.followers[i] = 0;
+          dataObj.sites[site].diff.following[i] = 0;
+
+          if ( i === 0 ) {
+            dataObj.sites[site].diff.followers[i] = followers;
+            dataObj.sites[site].diff.following[i] = following;
+          }
+        }
+
+        storage.set('user-data', dataObj);
+      },
+
+      // Clean total data
+      total: function(followers, following) {
+        for (var i = 0; i < dataObj.graph.followers.length; i++) {
+          dataObj.graph.followers[i] = 0;
+          dataObj.graph.following[i] = 0;
+
+          if ( i === 0 ) {
+            dataObj.graph.followers[i] = followers;
+            dataObj.graph.following[i] = following;
+          }
+        }
+
+        storage.set('user-data', dataObj);
+      }
+    }
+  },
 
   /**
    * Method to reload data and store on another object
    * the diff since the last app launched.
    */
   reload: function($this, site, followers, details) {
-    // Build data for graph
+    // Store followers/following data for building graph
     for (var i = 0; i < dataObj.graph.followers.length; i++) {
       if ( dataObj.graph.followers[i] === 0 ) {
         dataObj.sites[site].diff.followers[i] = followers;
@@ -112,7 +270,6 @@ var api = {
       }
     }
 
-    // Push to localstorage
     storage.set('user-data', dataObj);
 
     if ( dataObj.sites[site].followers !== followers ) {
@@ -160,43 +317,39 @@ var api = {
    * If there is no new followers for each networks, display an alert message
    */
   reloadCount: function() {
-    reload++;
+    api.reloadNbr++;
 
-    if ( Object.keys(dataObj.sites).length == reload ) api.alert(api.noUpdate);
+    if ( Object.keys(dataObj.sites).length === api.reloadNbr ) api.alert(api.noUpdate);
   },
 
   /**
-   * Method to sum up total of followers and following for all networks
+   * Method to ckeck if upgrade is needed to a new version of Statiks
    */
-  graph: function() {
-    $.each(dataObj.graph.followers, function(i, data) {
-      if ( data === 0 ) {
-        for (site in dataObj.sites) {
-          dataObj.graph.followers[i] += parseInt(dataObj.sites[site].diff.followers[i]);
-          dataObj.graph.following[i] += parseInt(dataObj.sites[site].diff.following[i]);
-        }
+  check: function() {
+    var v = storage.get('statiks-version');
+
+    if ( (v <= '1.1.0' || v === null) && storage.get('user-data') !== null ) {
+      tmpObj = storage.get('user-data');
+
+      for (var site in tmpObj) {
+        api[site]('upgrade', tmpObj[site].username, site);
+        delete tmpObj[site];
       }
-    });
 
-    storage.set('user-data', dataObj);
+      $(document).ajaxStop(function() {
+        api.graph();
+      });
+    }
+
+    app.version();
   },
 
   /**
-   * Method to upgrade old datas for upcoming version of the extension
+   * Method to upgrade with new data
    */
-  upgrade: function($this, site, followers, details) {
-    dataObj.sites[site].followers = followers;
-    dataObj.sites[site].details = details;
-    dataObj.sites[site].diff = 0;
-    storage.set('user-data', dataObj);
-
-    setTimeout(function() {
-      $('.loading').fadeOut(timingEffect);
-
-      setTimeout(function() {
-        data.build();
-      }, timingEffect);
-    }, timingEffect * 6);
+  upgrade: function($this, site, username, followers, details) {
+    $('.loading').fadeIn(timingEffect).find('p').text('Upgrade to the new version');
+    api.store(dataObj, site, username, followers, details);
   },
 
   /**
@@ -218,7 +371,7 @@ var api = {
         if ( $this === 'reload' ) {
           api.reload($this, site, followers, details);
         } else if ( $this === 'upgrade' ) {
-          api.upgrade($this, site, followers, details);
+          api.upgrade($this, site, username, followers, details);
         } else {
           api.success($this, site, username, followers, details);
         }
@@ -261,7 +414,7 @@ var api = {
         if ( $this === 'reload' ) {
           api.reload($this, site, followers, details);
         } else if ( $this === 'upgrade' ) {
-          api.upgrade($this, site, followers, details);
+          api.upgrade($this, site, username, followers, details);
         } else {
           api.success($this, site, username, followers, details);
         }
@@ -292,7 +445,7 @@ var api = {
         if ( $this === 'reload' ) {
           api.reload($this, site, followers, details);
         } else if ( $this === 'upgrade' ) {
-          api.upgrade($this, site, followers, details);
+          api.upgrade($this, site, username, followers, details);
         } else {
           api.success($this, site, username, followers, details);
         }
@@ -326,7 +479,7 @@ var api = {
         if ( $this === 'reload' ) {
           api.reload($this, site, followers, details);
         } else if ( $this === 'upgrade' ) {
-          api.upgrade($this, site, followers, details);
+          api.upgrade($this, site, username, followers, details);
         } else {
           api.success($this, site, username, followers, details);
         }
@@ -359,7 +512,7 @@ var api = {
         if ( $this === 'reload' ) {
           api.reload($this, site, followers, details);
         } else if ( $this === 'upgrade' ) {
-          api.upgrade($this, site, followers, details);
+          api.upgrade($this, site, username, followers, details);
         } else {
           api.success($this, site, username, followers, details);
         }
@@ -392,7 +545,7 @@ var api = {
         if ( $this === 'reload' ) {
           api.reload($this, site, followers, details);
         } else if ( $this === 'upgrade' ) {
-          api.upgrade($this, site, followers, details);
+          api.upgrade($this, site, username, followers, details);
         } else {
           api.success($this, site, username, followers, details);
         }
@@ -421,7 +574,7 @@ var api = {
         var getMedias = data.match(/\"media\":([^\,]+)/g);
 
         var username = value;
-        var followers = isMac ? getFollowers[1].substr(getFollowers[1].indexOf(':') + 1) : getFollowers[0].substr(getFollowers[0].indexOf(':') + 1);
+        var followers = (getFollowers.length == 2) ? getFollowers[1].substr(getFollowers[1].indexOf(':') + 1) : getFollowers[0].substr(getFollowers[0].indexOf(':') + 1);
 
         var details = {
           following: getFollowing[0].substr(getFollowing[0].indexOf(':') + 1),
@@ -431,7 +584,7 @@ var api = {
         if ( $this === 'reload' ) {
           api.reload($this, site, followers, details);
         } else if ( $this === 'upgrade' ) {
-          api.upgrade($this, site, followers, details);
+          api.upgrade($this, site, username, followers, details);
         } else {
           api.success($this, site, username, followers, details);
         }
@@ -471,7 +624,7 @@ var api = {
         if ( $this === 'reload' ) {
           api.reload($this, site, followers, details);
         } else if ( $this === 'upgrade' ) {
-          api.upgrade($this, site, followers, details);
+          api.upgrade($this, site, username, followers, details);
         } else {
           api.success($this, site, username, followers, details);
         }
@@ -499,7 +652,7 @@ var api = {
         if ( $this === 'reload' ) {
           api.reload($this, site, followers, details);
         } else if ( $this === 'upgrade' ) {
-          api.upgrade($this, site, followers, details);
+          api.upgrade($this, site, username, followers, details);
         } else {
           api.success($this, site, username, followers, details);
         }
@@ -533,7 +686,7 @@ var api = {
         if ( $this === 'reload' ) {
           api.reload($this, site, followers, details);
         } else if ( $this === 'upgrade' ) {
-          api.upgrade($this, site, followers, details);
+          api.upgrade($this, site, username, followers, details);
         } else {
           api.success($this, site, username, followers, details);
         }
@@ -567,7 +720,7 @@ var api = {
         if ( $this === 'reload' ) {
           api.reload($this, site, followers, details);
         } else if ( $this === 'upgrade' ) {
-          api.upgrade($this, site, followers, details);
+          api.upgrade($this, site, username, followers, details);
         } else {
           api.success($this, site, username, followers, details);
         }
